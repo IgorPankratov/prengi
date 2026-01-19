7cb7321af94d09ee2a305d11bebc2fd8723a9588
IgorP <ipankrat85@gmail.com>
2026-01-19 21:58:44 +0300

    Work with SMTP & mailer

--- src/js/index.js
+++ src/js/index.js
@@ -2,22 +2,35 @@
 const inner = document.querySelector(".slider__inner");
 const item = document.querySelectorAll(".slider__item");
 const prevBtn = document.querySelector(".slider__prev");
-const nextBtn = document.querySelector(".slider__next");    
+const nextBtn = document.querySelector(".slider__next");
 
 let currentIndex = 0;
 
 function show(index) {
-    inner.style.transform = `translateX(-${index * 100}%)`;
-    currentIndex = index;
+  inner.style.transform = `translateX(-${index * 100}%)`;
+  currentIndex = index;
 }
 
-prevBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + item.length) % item.length;
-    show(currentIndex);    
+prevBtn.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + item.length) % item.length;
+  show(currentIndex);
 });
 
-nextBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % item.length;
-    show(currentIndex);
+nextBtn.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % item.length;
+  show(currentIndex);
 });
 
+// Modal
+
+const consultationBtn = document.querySelectorAll(
+  '[data-modal="consultation"]'
+);
+const closeModal = document.querySelector(".modal__close");
+
+consultationBtn.forEach((item) => {
+  item.addEventListener("click", () => {
+    document.querySelector(".overlay").style.display = "block";
+    document.querySelector(".modal").style.display = "block";
+  });
+});
---
+++ src/mailer/smart.php
@@ -0,0 +1,66 @@
+<?php 
+
+// Устанавливаем заголовок Content-Type: application/json
+header('Content-Type: application/json');
+
+// Получаем данные из POST-запроса
+$name = $_POST['name'] ?? 'Не указано';
+$phone = $_POST['phone'] ?? 'Не указано';
+$email = $_POST['email'] ?? 'Не указано';
+
+// // Проверяем, что все поля заполнены
+// if (empty($name) || empty($phone) || empty($email)) {
+//     echo json_encode([
+//         'success' => false,
+//         'message' => 'Все поля обязательны для заполнения'
+//     ]);
+//     exit;
+// }
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'UTF-8';
+
+// Включите для отладки (уберите после теста!)
+$mail->SMTPDebug = 2;
+$mail->Debugoutput = 'html';                            // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'megapankrat@yandex.ru';                 // Наш логин
+$mail->Password = '';                           // Наш пароль от ящика либо пароль приложения
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('megapankrat@yandex.ru', 'Prengi');   // От кого письмо 
+$mail->addAddress('megapankrat@yandex.ru');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Данные';
+$mail->Body    = 'Пользователь оставил данные<br>' .
+    'Имя: ' . htmlspecialchars($name) . '<br>' .
+    'Номер телефона: ' . htmlspecialchars($phone) . '<br>' .
+    'E-mail: ' . htmlspecialchars($email);
+// Отправляем письмо и возвращаем JSON-ответ
+if ($mail->send()) {
+    echo json_encode([
+        'success' => true,
+        'message' => 'Письмо успешно отправлено'
+    ]);
+} else {
+    echo json_encode([
+        'success' => false,
+        'message' => 'Ошибка при отправке: ' . $mail->ErrorInfo
+    ]);
+}
+
+exit;
+
+?>
